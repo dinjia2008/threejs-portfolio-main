188811d57b00ff6880652e44ada069808dea885c
MCC <434512284@qq.com>
2024-09-10 11:23:31 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Hero from './sections/Hero.jsx';
+// import Hero from './sections/Hero.jsx';
 import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
